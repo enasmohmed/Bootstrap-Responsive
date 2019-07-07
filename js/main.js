34319a431a868f3618f66fd130ea45befe109b84
enas <enasm2477@gmail.com>
2019-07-07 21:18:10 +0200

    12

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function () {
+    'use strict';
+    $(window).scroll(function () {
+        var navbar = $('.navbar');
+        if ($(window).scrollTop() >= navbar.height()) {
+            navbar.addClass('scrolled');
+        } else {
+            navbar.removeClass('scrolled');
+        }          
+    });
+
+    // Deal With Tabs 
+    $('.tab-switch li').click(function () {
+        // Add Selected Class To Active Link
+        $(this).addClass('selected').siblings().removeClass('selected');
+        // Hide All Divs
+        $('.tabs-section .tabs-content > div').hide();
+        // Show Div Conncted With This Link 
+        $('.' + $(this).data('class')).show();
+    });
+});
